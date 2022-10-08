06c89125a52184a40efa7e916f130cca02f252d3
ihedvall <92839244+ihedvall@users.noreply.github.com>
2022-10-08 10:48:59 +0200

    Changed to FetchContent cmake build amd added gclang format file

--- script/expat.cmake
+++ script/expat.cmake
@@ -2,13 +2,18 @@
 # SPDX-License-Identifier: MIT
 
 if (NOT EXPAT_FOUND)
-    set(EXPAT_USE_STATIC_LIBS ON)
-    if (COMP_DIR)
-        set(EXPAT_ROOT ${COMP_DIR}//expat/master)
-    endif()
 
-    find_package(EXPAT REQUIRED)
+    find_package(EXPAT)
     message(STATUS "EXPAT Found (Try 1): "  ${EXPAT_FOUND})
+    if (NOT EXPAT_FOUND)
+        set(EXPAT_USE_STATIC_LIBS ON)
+        if (COMP_DIR)
+            set(EXPAT_ROOT ${COMP_DIR}/expat/master)
+        endif()
+
+        find_package(EXPAT REQUIRED)
+        message(STATUS "EXPAT Found (Try 2): "  ${EXPAT_FOUND})
+    endif()
     message(STATUS "EXPAT Include Dirs: "  ${EXPAT_INCLUDE_DIRS})
     message(STATUS "EXPAT Libraries: " ${EXPAT_LIBRARIES})
 endif()
