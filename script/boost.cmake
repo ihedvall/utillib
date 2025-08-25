781472f1549acb28d3d34eb63b94f2c0d085200b
ihedvall <92839244+ihedvall@users.noreply.github.com>
2025-08-25 11:48:50 +0200

    Build problem with boost

--- script/boost.cmake
+++ script/boost.cmake
@@ -1,15 +1,17 @@
 # Copyright 2021 Ingemar Hedvall
 # SPDX-License-Identifier: MIT
 
+if(POLICY CMP0167)
+    cmake_policy(SET CMP0167 OLD)
+endif()
+
 if (NOT Boost_FOUND)
     set(Boost_USE_STATIC_LIBS ON)
     set(Boost_USE_MULTITHREADED ON)
     set(Boost_ARCHITECTURE -x64)
     set(Boost_NO_WARN_NEW_VERSIONS ON)
     set(Boost_DEBUG OFF)
-    if(POLICY CMP0167)
-        cmake_policy(SET CMP0167 OLD)
-    endif()
+
     find_package(Boost COMPONENTS filesystem system locale program_options process)
     message(STATUS "Boost Found (Try 1): " ${Boost_FOUND})
     message(STATUS "Boost Version: " ${Boost_VERSION_STRING})
