1ff69f42fcaf75209cd8a2c6bb3638fc37c3aa21
ihedvall <92839244+ihedvall@users.noreply.github.com>
2026-02-03 12:13:17 +0100

    Added fix for boost and cmake

--- script/boost.cmake
+++ script/boost.cmake
@@ -1,6 +1,8 @@
 # Copyright 2021 Ingemar Hedvall
 # SPDX-License-Identifier: MIT
 
+include(CMakePrintHelpers)
+
 if (NOT Boost_FOUND)
     set(Boost_USE_STATIC_LIBS ON)
     set(Boost_USE_MULTITHREADED ON)
@@ -8,22 +10,18 @@ if (NOT Boost_FOUND)
     set(Boost_NO_WARN_NEW_VERSIONS ON)
     set(Boost_DEBUG OFF)
 
-    find_package(Boost COMPONENTS filesystem system locale program_options process)
-    message(STATUS "Boost Found (Try 1): " ${Boost_FOUND})
-    message(STATUS "Boost Version: " ${Boost_VERSION_STRING})
-    message(STATUS "Boost Include Dirs: " ${Boost_INCLUDE_DIRS})
-    message(STATUS "Boost Library Dirs: " ${Boost_LIBRARY_DIRS})
-    message(STATUS "Boost Libraries: " ${Boost_LIBRARIES})
-    message(STATUS "Boost Root: " ${Boost_ROOT})
+    find_package(Boost CONFIG COMPONENTS filesystem system locale program_options process)
     if (NOT Boost_FOUND)
-        set(Boost_ROOT ${COMP_DIR}/boost/latest)
-        find_package(Boost REQUIRED COMPONENTS filesystem system locale program_options process)
-
-        message(STATUS "Boost Found (Try 2): " ${Boost_FOUND})
-        message(STATUS "Boost Version: " ${Boost_VERSION_STRING})
-        message(STATUS "Boost Include Dirs: " ${Boost_INCLUDE_DIRS})
-        message(STATUS "Boost Library Dirs: " ${Boost_LIBRARY_DIRS})
-        message(STATUS "Boost Libraries: " ${Boost_LIBRARIES})
+        if (COMP_DIR)
+            set(Boost_ROOT ${COMP_DIR}/boost/latest/lib/cmake)
+        endif()
+        find_package(Boost REQUIRED CONFIG COMPONENTS filesystem system locale program_options process)
     endif()
 endif()
 
+cmake_print_variables(
+        Boost_FOUND
+        Boost_VERSION_STRING
+        Boost_INCLUDE_DIRS
+        Boost_LIBRARY_DIRS
+        Boost_LIBRARIES)
